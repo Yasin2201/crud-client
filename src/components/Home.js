073ae86051aca0c5c5fb117c3a0f8cf7dcba1d1f
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-30 15:10:10 +0100

    get all aritcles from api and set to articles state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react"
+
+
 const Home = () => {
+    const [articles, setArcticles] = useState()
+    useEffect(() => {
+        async function getAllArticles() {
+            try {
+                const res = await fetch('http://localhost:4000/', {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                const data = await res.json()
+                setArcticles(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getAllArticles()
+    }, [])
+
     return (
         <div>
             Home
