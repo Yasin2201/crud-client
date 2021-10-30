a0f78b4462b1c6a4626cc88638ae1a4cebd5a11b
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-30 19:48:41 +0100

    get new data from form and post valid data to database and redirect user to home onSubmit of form

--- src/components/NewArticle.js
+++ src/components/NewArticle.js
@@ -1,8 +1,39 @@
+import { useHistory } from "react-router";
+
 const NewArticle = () => {
+    let history = useHistory();
+
+    const onArticleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const userInput = {
+            title: formData.get('title'),
+            article: formData.get('article'),
+        }
+
+        try {
+            const response = await fetch(`http://localhost:4000/`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userInput)
+            })
+            const data = await response.json()
+            if (!data.error) {
+                history.push("/");
+            } else {
+                console.log(data)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
     return (
-        <form onSubmit={() => { console.log("Submitted") }}>
-            <input type='text' placeholder='New Article Title...' />
-            <textarea type='text' placeholder='Enter Article Text...' />
+        <form onSubmit={(e) => onArticleSubmit(e)}>
+            <input type='text' name="title" placeholder='New Article Title...' />
+            <textarea type='text' name="article" placeholder='Enter Article Text...' />
             <button type='submit'>Submit</button>
         </form>
     )
